7886792ebd30cc6d602d44679759184f0e7faa09
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-04 22:59:23 +0900

    feat: DETAILED_NAME flag

--- servers/v1/pack.sh
+++ servers/v1/pack.sh
@@ -2,5 +2,11 @@
 wd=$(mktemp -d)
 cp -r run/mods $wd/mods
 cp -r data/config $wd/config
-file_name="packed_$(TZ='Asia/Tokyo' date +%Y-%m-%d_%H.%M.%S)_git-$(git rev-parse HEAD).zip"
+if [[ "x$DETAILED_NAME" == "x" ]]; then
+  file_name="packed.zip"
+else
+  file_name="packed_$(TZ='Asia/Tokyo' date +%Y-%m-%d_%H.%M.%S)_git-$(git rev-parse HEAD).zip"
+fi
+
 (cd $wd && zip -9r $file_name .) && cp $wd/$file_name .
+rm -r $wd
