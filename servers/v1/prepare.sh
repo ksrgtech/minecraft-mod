d5cb0138f4b590be0e2b020aab1e053ec6ac0207
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-05 01:35:14 +0900

    feat: better build script

--- servers/v1/prepare.sh
+++ servers/v1/prepare.sh
@@ -16,16 +16,23 @@ function dl() {
 
 function curse_dl() {
   set -e
-  # shellcheck disable=SC2016
+  local x
   x=$(mktemp)
+  local file
+  if [[ "$SERVER" == "1" ]]; then
+    file="cf_mods_server.json"
+  elif [[ "$CLIENT" == "1" ]]; then
+    file="cf_mods_client.json"
+  fi
   # ✝✝✝!!!META-PROGRAMMING!!!✝✝✝
+  # shellcheck disable=SC2016
   curl \
       -q \
       -X POST \
       -H "Accept: application/json" \
       -H "Content-Type: application/json" \
       -H "x-api-key: $CURSE_API_KEY" \
-      -d@cf_mods.json \
+      -d@$file \
       https://api.curseforge.com/v1/mods/files \
     | jq \
       -r \
@@ -34,6 +41,7 @@ function curse_dl() {
       '.data[] | {fileId: .id, modId, fileName} | ("curl -qf -H \"Accept: application/json\" -H \u0027x-api-key: " + $curse + "\u0027 " + "https://api.curseforge.com/v1/mods/" + (.modId | tostring) + "/files/" + (.fileId | tostring) + "/download-url" + " | " + "jq -r \u0027.data\u0027" + " | " + "tr -d \u0027\\n\u0027" + " | " + "xargs -0 -i wget {} -O \"" + $dir + "/" + .fileName + "\"")' \
     | tee "$x" >/dev/null # Yeah, this is a security risk
   chmod +x "$x"
+  # syntax validation
   /bin/bash -n "$x"
   $x
   ls run/mods
@@ -45,6 +53,9 @@ DIR=run
 
 [ -z "$DIR" ] && die "\$DIR can not be empty"
 [ ! -d "$DIR" ] && mkdir -p "$DIR"
+[[ "$SERVER" == "" && "$CLIENT" == "" ]] && die "You must specify \$SERVER or \$CLIENT"
+[[ "$SERVER" == "1" && "$CLIENT" == "1" ]] && die "You must not specify both \$SERVER and \$CLIENT"
+
 if [ -z "$CURSE_API_KEY" ]; then
   die "CFCore token must be provided via \$CURSE_API_KEY"
 else
@@ -55,7 +66,10 @@ MOD_DIR=$DIR/mods
 rm -r $MOD_DIR || true
 mkdir -p $MOD_DIR
 
-dl "https://optifine.net/downloadx?f=OptiFine_1.12.2_HD_U_G5.jar&x=79875e46fbbc1166cb1d14b96c5a684a" "OptiFine_1.12.2_HD_U_G5"
+if [[ "$CLIENT" == "1" ]]; then
+  dl "https://optifine.net/downloadx?f=OptiFine_1.12.2_HD_U_G5.jar&x=79875e46fbbc1166cb1d14b96c5a684a" "OptiFine_1.12.2_HD_U_G5"
+fi
+
 dl "https://www.dropbox.com/sh/mlfsx6b3z5ek8wv/AACCf_0tDiPo8fd2rwa0CoEia/SpawnChecker/Minecraft_1.12.x/SpawnChecker-2.7.7.137.jar?dl=1" "SpawnChecker-2.7.7.137"
 dl "https://web.archive.org/web/20190715131820/https://forum.minecraftuser.jp/download/file.php?id=75930" "StorageBox-3.2.0"
 dl "https://github.com/KisaragiEffective/publicfile/blob/master/RTG-1.12.2-6.1.0.0-snapshot.2+flavored.ksrg.git-b7769d2dc6d0941922a26090dd1c15328eb4d1d0?raw=true" "RTG-1.12.2-6.1.0.0-snapshot.2+flavored.ksrg.git-b7769d2dc6d0941922a26090dd1c15328eb4d1d0"
