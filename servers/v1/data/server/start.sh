49f298a932e6e1cc98e9e974672e77bfa11efc7e
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-15 16:33:16 +0900

    feat: don't do that. avoiding.

--- servers/v1/data/server/start.sh
+++ servers/v1/data/server/start.sh
@@ -4,6 +4,8 @@ readonly SERVERDIR=`dirname $0`
 cd ${SERVERDIR}
 
 PASSWORD=$(cat the-secret-password-do-not-leak-to-external.txt) /home/mcserver/mc/decrypt.sh whitelist.json.asc > whitelist.json
+# https://github.com/ksrgtech/minecraft-mod/issues/108
 /usr/lib/jvm/temurin-8-jdk-arm64/bin/java \
        -Xmx10G \
+       -Dfml.queryResult=confirm \
        -jar /home/mcserver/mc/minecraft_server.1.12.2.jar
