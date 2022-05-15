4de30ef5fc58e5e07db337948318f2f2030a0307
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-15 10:50:46 +0900

    fix: decryptをstdoutに出力しない

--- servers/v1/data/server/start.sh
+++ servers/v1/data/server/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/mcserver/mc/decrypt.sh whitelist.json.asc
+/home/mcserver/mc/decrypt.sh whitelist.json.asc > whitelist.json &&
 /usr/bin/screen -Dm -S mc /usr/bin/java -Xmx10G -jar /home/mcserver/mc/server.jar
