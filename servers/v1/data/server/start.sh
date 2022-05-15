7fe91125457719e8454e4b39427a5ffc7df0b067
KisaragiEffective <kisaragi.effective@gmail.com>
2022-05-15 11:10:38 +0900

    fix: passwordを与える

--- servers/v1/data/server/start.sh
+++ servers/v1/data/server/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/mcserver/mc/decrypt.sh whitelist.json.asc > whitelist.json &&
+PASSWORD=$(cat the-secret-password-do-not-leak-to-external.txt) /home/mcserver/mc/decrypt.sh whitelist.json.asc > whitelist.json &&
 /usr/bin/screen -Dm -S mc /usr/bin/java -Xmx10G -jar /home/mcserver/mc/server.jar
